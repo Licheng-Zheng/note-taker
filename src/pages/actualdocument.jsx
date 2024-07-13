f720f4c3af0daa065cee25e1b1957d9639e708f3
Matthew Yu <matthewyu30121@gmail.com>
2024-07-13 04:29:51 -0400

    i give up

--- src/pages/actualdocument.jsx
+++ src/pages/actualdocument.jsx
@@ -3,10 +3,10 @@ import NavBar from '../components/Navbar'
 
 export default function ActualDocument(){
 return (
-
     <div>
-    <NavBar/>
-
+        <NavBar />
+        <h1>Note Taker</h1>
+        <textarea rows="10" cols="50" placeholder="Start taking notes..."></textarea>
     </div>
 );
 
