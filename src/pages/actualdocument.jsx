ee72f8ebc774f49373151a69eba3b44b1017741e
Matthew Yu <matthewyu30121@gmail.com>
2024-07-13 18:45:57 -0400

    further front-end work, now i am going to start backend

--- src/pages/actualdocument.jsx
+++ src/pages/actualdocument.jsx
@@ -1,12 +1,28 @@
 import NavBar from '../components/Navbar'
+import './styles/actual.css';
+function Save(){
+    return (
+        <button className='special'>Save</button>
+    );
+}
+function ShareUrl(){
+    return (
+        <button className='special'>Share URL</button>
+    );
 
-
-export default function ActualDocument(){
+}
+export default function ActualDocument({NameDoc, Content}){
 return (
     <div>
         <NavBar />
         <h1>Note Taker</h1>
-        <textarea rows="10" cols="50" placeholder="Start taking notes..."></textarea>
+
+        <div className = 'container'>
+        <Save/>
+        <ShareUrl/>
+        </div>
+       
+        <textarea className = 'free' placeholder="Start taking notes..."></textarea>
     </div>
 );
 
--- src/pages/notes.jsx
+++ src/pages/notes.jsx
@@ -5,15 +5,35 @@ function Documents({NameNotes, Content}){
     <>
 <div className = "container">
     <h3>{NameNotes}</h3>
-    <button>Edit</button>
-    <button>Delete</button>
-    <button>Share URL</button>
+    <button className='average'>Edit</button>
+    <button className ='average'>Delete</button>
+    <button className ='average'>Share URL</button>
     </div>    </>
     );
 }
+ function Templates({Name}){
+
+    return(
+    <>
+    <div className = "container">
+    <h3>{Name}</h3>
+    <button className = 'average'>Use Template</button>
+    </div>
+    </>
+    );
+ }
 function  ContainerDocument(){
     return(
-    <div className = 'containdocs'>
+        <>
+        <h2 class = 'notetitles'>Templates to use:</h2>
+        <div className = 'containdocs'>
+    <Templates Name="Cornell-Notes"/>
+    <Templates Name="Outline-Notes"/>       
+    <Templates Name="Free-Notes"/>
+        </div>
+        <hr/>
+        <h2 class = 'notetitles'>Your saved work.</h2>
+        <div className='containdocs'>
     <Documents NameNotes="Note 1" Content="Content 1"/>
     <Documents NameNotes="Note 2" Content="Content 2"/>
     <Documents NameNotes="Note 3" Content="Content 3"/>
@@ -21,6 +41,8 @@ function  ContainerDocument(){
     <Documents NameNotes="Note 5" Content="Content 5"/>
     <Documents NameNotes="Note 6" Content="Content 5"/>
     </div>
+    
+        </>
     
     );
 
